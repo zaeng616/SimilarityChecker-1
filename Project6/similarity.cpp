946cf265920cb639b4681b2dff6ebead1507172f
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-23 15:28:47 +0900

    refactoring for value

--- Project6/similarity.cpp
+++ Project6/similarity.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <stdlib.h>
 class Similiarity {
+	const int MAX_LENGTH_SIMILARITY = 60;
 public:
 	int GetLengthSimilarity(std::string a, std::string b) {
 		if (a.length() == b.length())
-			return 60;
+			return MAX_LENGTH_SIMILARITY;
+		return 0;
 	}
+
 };
