625b4f7887d60214990f9c404c555b29e18f5733
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-23 15:27:43 +0900

    add length similarity

--- Project6/similarity.cpp
+++ Project6/similarity.cpp
@@ -2,5 +2,8 @@
 #include <stdlib.h>
 class Similiarity {
 public:
-
+	int GetLengthSimilarity(std::string a, std::string b) {
+		if (a.length() == b.length())
+			return 60;
+	}
 };
--- Project6/similarity_test.cpp
+++ Project6/similarity_test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "gmock/gmock.h"
 #include "similarity.cpp"
-
+TEST(SimilarityTest, TestLength) {
+	Similiarity s;
+	EXPECT_EQ(60, s.GetLengthSimilarity("ASD", "DSA"));
+}
 int main() {
 	testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
