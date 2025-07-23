4fc8e42d20ca184f1d07752f9e2036760f9321e3
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-23 15:26:00 +0900

    initial commit

---
+++ Project6/similarity.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <stdlib.h>
+class Similiarity {
+public:
+
+};
---
+++ Project6/similarity_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "gmock/gmock.h"
+#include "similarity.cpp"
+
+int main() {
+	testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
