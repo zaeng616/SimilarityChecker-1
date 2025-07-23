9dba6c7e5e507b751877170f3d05e47eaccdc3df
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-23 15:30:15 +0900

    add length fail tc

--- Project6/similarity_test.cpp
+++ Project6/similarity_test.cpp
@@ -5,6 +5,10 @@ TEST(SimilarityTest, TestLength) {
 	Similiarity s;
 	EXPECT_EQ(60, s.GetLengthSimilarity("ASD", "DSA"));
 }
+TEST(SimiliarityTest, TestLengthFail) {
+	Similiarity s;
+	EXPECT_EQ(0, s.GetLengthSimilarity("A", "BB"));
+}
 int main() {
 	testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
