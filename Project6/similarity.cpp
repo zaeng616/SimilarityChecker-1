94be019787f2f3e18c8bc70dd7aa9dd7e935886d
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-23 17:02:46 +0900

    refactoring for alpha similarity

--- Project6/similarity.cpp
+++ Project6/similarity.cpp
@@ -1,42 +1,39 @@
 #include <string>
 #include <stdlib.h>
+#include <unordered_set>
 class Similiarity {
 	const int MAX_LENGTH_SIMILARITY = 60;
+	const int MAX_ALPHA_SIMILARITY = 40;
 public:
 	int GetLengthSimilarity(const std::string& a, const std::string& b) {
 		if (a.length() == b.length())
 			return MAX_LENGTH_SIMILARITY;
 		if (a.length() > b.length() * 2 || a.length() * 2 < b.length())
 			return 0;
-		return CalculatePartialLength(a.length(), b.length());
+		return calculatePartialLength(a.length(), b.length());
 	}
+
 	int GetAlphaSImilarity(const std::string& a, const std::string& b) {
-		int i, j;
+		int totalCnt = getPureAlpha(a + b).length();	
 		int sameCnt = 0;
-		int totalCnt = 0;
-		//total cnt
-		bool total_arr[26] = {};
-		bool same_arr[26] = {};
-		for (int i = 0; i < a.length(); i++)
-			total_arr[a[i] - 'A'] = true;
-		for (int i = 0; i < b.length(); i++)
-			total_arr[b[i] - 'A'] = true;
-		for (int i = 0; i < sizeof(total_arr) / sizeof(bool); i++)
-			if (total_arr[i]) totalCnt++;
-		for (i = 0; i < a.length(); i++) {
-			for (j = 0; j < b.length(); j++)
-				if (a[i] == b[j]) {
-					same_arr[a[i] - 'A'] = true;
-					break;
-				}
-		}
-		for (int i = 0; i < sizeof(same_arr) / sizeof(bool); i++)
-			if (same_arr[i]) sameCnt++;
-		return (double)sameCnt/totalCnt*40;
+		for (auto ch : getPureAlpha(a))
+			if (b.find(ch) != std::string::npos)
+				sameCnt++;
+		return (double)sameCnt/totalCnt* MAX_ALPHA_SIMILARITY;
 	}
 
 private:
-	int CalculatePartialLength(int length_a, int length_b) {
+	int calculatePartialLength(int length_a, int length_b) {
 		return (1 - ((double)std::abs(length_a-length_b) / std::min(length_a, length_b))) * MAX_LENGTH_SIMILARITY;
+	}
+	std::string getPureAlpha(const std::string& str) {
+		std::string ret = "";
+		std::unordered_set<char> pureCheck{};
+		for (const auto ch : str)
+			if (!pureCheck.count(ch)) {
+				ret += ch;
+				pureCheck.insert(ch);
+			}
+		return ret;
 	}
 };
